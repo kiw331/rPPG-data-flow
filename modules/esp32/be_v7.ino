9b625f40be3b2ecb19180d5f47d9c94d07a97bf1
kiwi <mingureion@gmail.com>
2026-05-13 11:15:45 +0900

    Red Value도 전송, 기록되도록 수정

--- modules/esp32/be_v7.ino
+++ modules/esp32/be_v7.ino
@@ -1,34 +1,41 @@
 /*
- * ESP32 PPG Monitor (Dual Core + Binary Protocol)
- * - Protocol: [0xA5][0x5A][SEQ(1)][IR(4)] = Total 7 Bytes
+ * ESP32 PPG Monitor (Dual Core + Binary Protocol) - v7
+ * - Protocol: [0xA5][0x5A][SEQ(1)][IR(4)][RED(4)] = Total 11 Bytes
+ * - Brightness command (ASCII, '\n' terminated):
+ *     "I<val>"  -> IR  LED amplitude (0~255)
+ *     "R<val>"  -> RED LED amplitude (0~255)
+ *     "<val>"   -> (legacy) IR LED amplitude
  */
 
 #include <Wire.h>
-#include "MAX30105.h" 
+#include "MAX30105.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
 // ===== [설정] =====
-#define I2C_SPEED        400000  
+#define I2C_SPEED        400000
 #define SERIAL_BAUD      115200  // 바이너리라 115200으로도 충분함 (물론 921600 쓰면 더 좋음)
-#define QUEUE_SIZE       256     
+#define QUEUE_SIZE       256
+#define PACKET_SIZE      11
 
 MAX30105 particleSensor;
 QueueHandle_t ppgQueue;
-volatile int commandBrightness = -1; 
+volatile int commandBrightnessIR  = -1;
+volatile int commandBrightnessRed = -1;
 
 // 큐 데이터 구조체
 struct PpgPacket {
   uint32_t ir;
-  uint8_t seq;
+  uint32_t red;
+  uint8_t  seq;
 };
 
 // ======================== [Core 0] Binary Tx Task ========================
 void TaskSerialTx(void *pvParameters) {
   PpgPacket packet;
-  // 바이너리 패킷 버퍼: [Header1][Header2][Seq][IR_4bytes]
-  uint8_t txBuf[7]; 
+  // 바이너리 패킷 버퍼: [Header1][Header2][Seq][IR_4bytes][RED_4bytes]
+  uint8_t txBuf[PACKET_SIZE];
   txBuf[0] = 0xA5; // Sync Byte 1
   txBuf[1] = 0x5A; // Sync Byte 2
 
@@ -36,25 +43,42 @@ void TaskSerialTx(void *pvParameters) {
     if (xQueueReceive(ppgQueue, &packet, (TickType_t)10) == pdPASS) {
       // 데이터 채우기
       txBuf[2] = packet.seq;
-      
+
       // IR값(4바이트) 쪼개서 넣기 (Little Endian)
       txBuf[3] = (uint8_t)(packet.ir & 0xFF);
       txBuf[4] = (uint8_t)((packet.ir >> 8) & 0xFF);
       txBuf[5] = (uint8_t)((packet.ir >> 16) & 0xFF);
       txBuf[6] = (uint8_t)((packet.ir >> 24) & 0xFF);
 
-      // 7바이트 한 번에 전송 (가장 효율적)
-      Serial.write(txBuf, 7);
-      
+      // RED값(4바이트) 쪼개서 넣기 (Little Endian)
+      txBuf[7]  = (uint8_t)(packet.red & 0xFF);
+      txBuf[8]  = (uint8_t)((packet.red >> 8) & 0xFF);
+      txBuf[9]  = (uint8_t)((packet.red >> 16) & 0xFF);
+      txBuf[10] = (uint8_t)((packet.red >> 24) & 0xFF);
+
+      // 11바이트 한 번에 전송 (가장 효율적)
+      Serial.write(txBuf, PACKET_SIZE);
+
     } else {
       vTaskDelay(1);
     }
 
-    // [밝기 명령 수신] 기존과 동일
+    // [밝기 명령 수신] "I<val>" / "R<val>" / "<val>"(legacy=IR)
     if (Serial.available() > 0) {
-      int val = Serial.parseInt();
-      while(Serial.available()) Serial.read();
-      if (val >= 0 && val <= 255) commandBrightness = val;
+      char prefix = Serial.peek();
+      if (prefix == 'I' || prefix == 'R') {
+        Serial.read(); // prefix 소비
+        int val = Serial.parseInt();
+        while (Serial.available()) Serial.read();
+        if (val >= 0 && val <= 255) {
+          if (prefix == 'I') commandBrightnessIR = val;
+          else               commandBrightnessRed = val;
+        }
+      } else {
+        int val = Serial.parseInt();
+        while (Serial.available()) Serial.read();
+        if (val >= 0 && val <= 255) commandBrightnessIR = val;
+      }
     }
   }
 }
@@ -63,7 +87,7 @@ void TaskSerialTx(void *pvParameters) {
 void setup() {
   Serial.begin(SERIAL_BAUD);
   Wire.begin();
-  
+
   ppgQueue = xQueueCreate(QUEUE_SIZE, sizeof(PpgPacket));
 
   if (!particleSensor.begin(Wire, I2C_SPEED)) {
@@ -71,12 +95,12 @@ void setup() {
     while (1);
   }
 
-  byte ledBrightness = 60; 
-  byte sampleAverage = 1; 
-  byte ledMode = 2;        
-  int sampleRate = 200;    
-  int pulseWidth = 411;    
-  int adcRange = 4096;     
+  byte ledBrightness = 60;
+  byte sampleAverage = 1;
+  byte ledMode = 2;        // 2 = Red + IR
+  int sampleRate = 200;
+  int pulseWidth = 411;
+  int adcRange = 4096;
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
   particleSensor.enableFIFORollover();
@@ -87,20 +111,25 @@ void setup() {
 void loop() {
   static uint8_t sequence = 0; // 0~255 자동 순환
 
-  if (commandBrightness != -1) {
-    particleSensor.setPulseAmplitudeIR((byte)commandBrightness);
-    commandBrightness = -1;
+  if (commandBrightnessIR != -1) {
+    particleSensor.setPulseAmplitudeIR((byte)commandBrightnessIR);
+    commandBrightnessIR = -1;
   }
-  
+  if (commandBrightnessRed != -1) {
+    particleSensor.setPulseAmplitudeRed((byte)commandBrightnessRed);
+    commandBrightnessRed = -1;
+  }
+
   particleSensor.check();
 
   while (particleSensor.available()) {
     PpgPacket packet;
-    packet.ir = particleSensor.getFIFOIR(); 
+    packet.ir  = particleSensor.getFIFOIR();
+    packet.red = particleSensor.getFIFORed();
     packet.seq = sequence++; // 0~255, 255 다음엔 0으로
-    
+
     particleSensor.nextSample();
-    xQueueSend(ppgQueue, &packet, 0); 
+    xQueueSend(ppgQueue, &packet, 0);
   }
-  delay(1); 
+  delay(1);
 }
